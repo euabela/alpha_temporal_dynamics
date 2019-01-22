5823b553fe80bd2bd3d4fb1cb99430b19d79370d
Eugenio Abela <abela.eugenio@gmail.com>
2019-01-22 14:02:06 +0000

    Updated comments

--- functions/td_modelpsd.m
+++ functions/td_modelpsd.m
@@ -1,5 +1,24 @@
 function model = td_modelpsd(params,freq)
-% Model psd based on fitted parameters
+% Models psd based on fitted parameters
+%
+% INPUTS
+% - params: parameter structure for model
+% - freq: frequency vector
+%
+% OUTPUTS
+% - model structure with fields
+%       .back - vector of data values for fitted 1/f background
+%       .osc  - vector of data values for fitted oscillation
+%       .freq - vector of frequencies
+%
+% DEPENDENCIES
+% - FieldTrip
+%
+% USAGE
+% >>  model = td_modelpsd(params,2:0.1:20);
+%
+%--------------------------------------------------------------------------
+% (c) Eugenio Abela, MD / Richardson Lab
 
 if nargin <2
     freq = 2:0.1:24;
@@ -20,3 +39,4 @@ osc = pk*exp(-((freq-cf)/sd).^2);
 % Pack up
 model.back = back;
 model.osc  = osc;
+model.freq = freq;
